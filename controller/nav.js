15271e3859f0b7a8f246dcb75b7e24a4d15d31f7
MinuraWije <minurawije@gmail.com>
2024-12-06 22:20:59 +0530

    updated nav home click

--- controller/nav.js
+++ controller/nav.js
@@ -4,9 +4,15 @@ $('#nav-home').on('click',() =>  {
     $('#order-section').css({display: 'none'});
     $('#order-details-section').css({display: 'none'});*/
 
-    $("#home-section").css('display','block')
-    $("#customer-section").css('display','none')
-    $("#item-section").css('display','none')
-    $("#order-details-section").css('display','none')
-    $("#order-section").css('display','none')
+
+    $("#header-section").css('display','block')
+    $("#dashboard-section").css('display','block')
+    $("#login-section").css('display','none')
+    $("#signup-section").css('display','none')
+    $("#field-section").css('display','none')
+    $("#crop-section").css('display','none')
+    $("#log-section").css('display','none')
+    $("#staff-section").css('display','none')
+    $("#equipment-section").css('display','none')
+    $("#vehicle-section").css('display','none')
 });
